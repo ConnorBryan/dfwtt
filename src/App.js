612f5657bcbdf9b93dff017d650b57b6c7b1d9f7
Connor Bryan <cchromium@gmail.com>
2017-07-13 18:02:41 -0400

    Added locations and refactored repetitious stuff out

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import { Header, Button, Icon, List, Card, Image } from 'semantic-ui-react';
+import { Header, Button, Icon, List, Card, Image, Message } from 'semantic-ui-react';
 
 const DFWTT = {
+  logo: '/dfwtt.gif',
+  title: 'Dallas/Fort Worth Table Tennis',
+  email: 'dfwtt1@gmail.com',
+  phone: '(214) 240-5975',
+  pages: [
+    {
+      title: 'News',
+      path: '/',
+      value: 0,
+    },
+    {
+      title: 'Locations',
+      path: '/locations',
+      value: 1,
+    },
+    {
+      title: 'Coaching',
+      path: '/coaching',
+      value: 2,
+    },
+    {
+      title: 'Membership',
+      path: '/membership',
+      value: 3,
+    },
+  ],
   news: [
     {
       image: '/2017dfwttspringopen.jpg',
@@ -22,7 +48,77 @@ const DFWTT = {
       ],
     },
   ],
+  locations: [
+    {
+      image: '/cimarron.jpg',
+      header: 'Cimarron Recreation Center',
+      address: '265 Red River Trail, Irving, TX 75063',
+      schedule: [
+        {
+          day: 'Monday',
+          time: '7:30pm to 9:30pm',
+        },
+        {
+          day: 'Saturday',
+          time: '9:00am to 12:00pm',
+        },
+      ],
+      directions: 'http://maps.google.com/maps?um=1&tab=wl&hl=en&q=265%20Red%20River%20Trail%20TX',
+    },
+    {
+      image: '/jcc.jpg',
+      header: 'Jewish Community Center',
+      address: '7900 Northaven Road, Dallas, TX 75230',
+      notice: {
+        header: 'Parking Notice',
+        content: 'Use upper level parking lots only and use the Health & Fitness Complex entrance door located on North side of building. Sign in at front desk.',
+      },
+      schedule: [
+        {
+          day: 'Monday',
+          time: '6:30pm to 9:30pm',
+        },
+        {
+          day: 'Wednesday',
+          time: '6:30pm to 9:30pm',
+        },
+      ],
+      directions: 'http://maps.google.com/maps?f=q&hl=en&geocode=&q=7900+Northaven+Road,+Dallas+75230&sll=37.0625,-95.677068&sspn=45.014453,81.738281&ie=UTF8&z=16&iwloc=addr&om=1',
+    },
+    {
+      image: '/tbarm.jpg',
+      header: 'T Bar M Racquet Club',
+      address: '6060 Dilbeck Ln, Dallas, TX 75240',
+      schedule: [
+        {
+          day: 'Saturday',
+          time: '2:00pm to 5:00pm',
+        },
+        {
+          day: 'Sunday',
+          time: '2:00pm to 5:00pm',
+        },
+      ],
+      directions: 'https://www.google.com/maps/place/6060+Dilbeck+Ln,+Dallas,+TX+75240/@32.9292,-96.8036111,17z/data=!3m1!4b1!4m5!3m4!1s0x864c20fa8be63af1:0x96164675e63cb247!8m2!3d32.9292!4d-96.8014224?hl=en',
+    },
+  ],
 };
+
+const NavPanel = props => (
+  <div className="NavPanel">
+    <List divided relaxed="very">
+      {DFWTT.pages.map((page, i) => (
+        <List.Item
+          className={`${i === DFWTT.pages.length - 1 ? 'NavPanel-lastItem' : 'NavPanel-item'} ${props.getActiveRoute() === page.value ? 'NavPanel-item__active' : ''}`}
+          onClick={() => props.clickThroughListItem(page.title)}>
+          <List.Content className="NavPanel-content">
+            <Link to={page.path} onClick={props.toggleNavPanel} id={page.title}>{page.title}</Link>
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  </div>
+);
 
 const News = () => (
   <div className="News">
@@ -57,6 +153,7 @@ const News = () => (
     </Card.Group>
   </div>
 );
+
 const Locations = () => (
   <div className="Locations">
     <Header
@@ -65,20 +162,50 @@ const Locations = () => (
       Locations
     </Header>
     <Card.Group stackable doubling>
-      <Card>
-        <Image src='http://placehold.it/400x400' />
-        <Card.Content>
-          <Card.Header>
-            News
-          </Card.Header>
-          <Card.Meta>
-            Today's date
-          </Card.Meta>
-          <Card.Description>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur qui deleniti asperiores, nesciunt odio quas expedita dolor consequuntur voluptates excepturi perferendis nihil sed consequatur velit et nemo! Veniam quidem, ex.
-          </Card.Description>
-        </Card.Content>
-      </Card>
+      {DFWTT.locations.map(location => (
+        <Card>
+          <Image src={location.image} />
+          <Card.Content>
+            <Card.Header>
+              {location.header}
+            </Card.Header>
+            <Card.Meta>
+              {location.address}
+            </Card.Meta>
+          </Card.Content>
+          {location.notice && (
+            <Card.Content extra>
+              <Message compact>
+                <Message.Header>
+                  {location.notice.header}
+                </Message.Header>
+                {location.notice.content}
+              </Message>
+            </Card.Content>
+          )}
+          <Card.Content extra>
+            <Card.Description>
+              <Header as="h4">
+                Schedule
+              </Header>
+              <List>
+                {location.schedule.map(day => (
+                  <List.Item>
+                    <strong>{day.day}:</strong> {day.time}
+                  </List.Item>
+                ))}
+              </List>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a href={location.directions}>
+              <Button fluid>
+                Get Directions
+              </Button>
+            </a>
+          </Card.Content>
+        </Card>
+      ))}
     </Card.Group>
   </div>
 );
@@ -117,58 +244,28 @@ class App extends Component {
         <div className="Header">
           <Header
             as="h3"
-            image="/dfwtt.gif"
-            content="Dallas/Fort Worth Table Tennis" />
+            image={DFWTT.logo}
+            content={DFWTT.title} />
         </div>
         <div className="Contact">
           <Button.Group fluid>
             <Button>
               <Icon name="mail outline" />
-              dfwtt1@gmail.com
+              {DFWTT.email}
             </Button>
             <Button>
               <Icon name="text telephone" />
-              (214) 677-6265
+              {DFWTT.phone}
             </Button>
           </Button.Group>
         </div>
         <Router>
           <div className="Content">
             {this.state.navPanelShowing
-              ? (
-                <div className="NavPanel">
-                  <List divided relaxed="very">
-                    <List.Item
-                      className={`NavPanel-item ${this.getActiveRoute() === 0 ? 'NavPanel-item__active' : ''}`}
-                      onClick={() => this.clickThroughListItem('News')}>
-                      <List.Content className="NavPanel-content">
-                        <Link to="/" onClick={this.toggleNavPanel}  id="News">News</Link>
-                      </List.Content>
-                    </List.Item>
-                    <List.Item 
-                      className={`NavPanel-item ${this.getActiveRoute() === 1 ? 'NavPanel-item__active' : ''}`} 
-                      onClick={() => this.clickThroughListItem('Locations')}>
-                      <List.Content className="NavPanel-content">
-                        <Link to="/locations" onClick={this.toggleNavPanel} id="Locations">Locations</Link>
-                      </List.Content>
-                    </List.Item>
-                    <List.Item
-                      className={`NavPanel-item ${this.getActiveRoute() === 2 ? 'NavPanel-item__active' : ''}`}
-                      onClick={() => this.clickThroughListItem('Coaching')}>
-                      <List.Content className="NavPanel-content">
-                        <Link to="/coaching" onClick={this.toggleNavPanel}  id="Coaching">Coaching</Link>
-                      </List.Content>
-                    </List.Item>
-                    <List.Item
-                      className={`NavPanel-lastItem ${this.getActiveRoute() === 3 ? 'NavPanel-item__active' : ''}`}
-                      onClick={() => this.clickThroughListItem('Membership')}>
-                      <List.Content className="NavPanel-content">
-                        <Link to="/membership" onClick={this.toggleNavPanel}  id="Membership">Membership</Link>
-                      </List.Content>
-                    </List.Item>
-                  </List>
-                </div>
-              )
+              ? <NavPanel
+                getActiveRoute={this.getActiveRoute} 
+                clickThroughListItem={this.clickThroughListItem}
+                toggleNavPanel={this.toggleNavPanel} />
               : (
                 <div className="Content-routes">
                   <Route exact path="/" component={News} />
