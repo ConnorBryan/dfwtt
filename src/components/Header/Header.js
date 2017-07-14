2c0e385bb89b1d80b239edc37ee250048534dafb
Connor Bryan <cchromium@gmail.com>
2017-07-13 22:48:31 -0400

    Clean up the mobile header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header } from 'semantic-ui-react';
+import { Header, Image, Menu } from 'semantic-ui-react';
 import './Header.css';
 
 export default props => (
   <div className="Header">
-    <Link to='/'>
-        <Header
-          as="h3"
-          image={props.logo}
-          content={props.title} />
-    </Link>
+    <Menu
+      className="Header-menu"
+      attached="top"
+      fluid>
+      <Menu.Item>
+        <Link to="/">
+          <Image src={props.logo} size="tiny" />
+        </Link>
+      </Menu.Item>
+      <Link
+        className="Header-title"
+        to="/">
+          <Header as="h3">
+            {props.title}
+          </Header>
+        </Link>
+    </Menu>
   </div>
 );
